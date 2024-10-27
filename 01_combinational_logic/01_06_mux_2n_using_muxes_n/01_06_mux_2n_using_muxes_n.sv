4f88b11a80644bf9bb79364f60c396aacb65c9fc
Emptea <aryas1725@gmail.com>
2024-10-27 11:53:20 +0300

    hw1 done

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -23,9 +23,13 @@ module mux_4_1
   input  [1:0] sel,
   output [3:0] y
 );
-
+  wire [3:0] sel1;
+  wire [3:0] sel0;
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
-
+  mux_2_1 mux0 (.d0(d0), .d1(d1), .sel(sel[0]), .y(sel0));
+  mux_2_1 mux1 (.d0(d2), .d1(d3), .sel(sel[0]), .y(sel1));
+  
+  mux_2_1 mux_out (.d0(sel0), .d1(sel1), .sel(sel[1]), .y(y));
 
 endmodule
--- 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
+++ 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
@@ -27,6 +27,9 @@ module xor_gate_using_mux
   // Task:
   // Implement xor gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
-
+  wire or_out, and_out;
+  mux mux_or(b,1'b1, a, or_out);
+  mux mux_and (1'b0, a, b, and_out);
+  mux mux_out(or_out, '0, and_out, o);
 
 endmodule
