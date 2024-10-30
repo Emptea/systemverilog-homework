bb5bec30f6faccaa56a147eb8951b4b56f3bb34c
Emptea <aryas1725@gmail.com>
2024-10-31 00:47:02 +0300

    pass3

--- 02_sequential_basics/02_03_serial_adder_using_logic_operations_only/02_03_serial_adder_using_logic_operations_only.sv
+++ 02_sequential_basics/02_03_serial_adder_using_logic_operations_only/02_03_serial_adder_using_logic_operations_only.sv
@@ -2,28 +2,25 @@
 // Example
 //----------------------------------------------------------------------------
 
-module serial_adder
-(
-  input  clk,
-  input  rst,
-  input  a,
-  input  b,
-  output sum
+module serial_adder (
+    input  clk,
+    input  rst,
+    input  a,
+    input  b,
+    output sum
 );
 
   // Note:
   // carry_d represents the combinational data input to the carry register.
 
   logic carry;
-  wire carry_d;
+  wire  carry_d;
 
-  assign { carry_d, sum } = a + b + carry;
+  assign {carry_d, sum} = a + b + carry;
 
-  always_ff @ (posedge clk)
-    if (rst)
-      carry <= '0;
-    else
-      carry <= carry_d;
+  always_ff @(posedge clk)
+    if (rst) carry <= '0;
+    else carry <= carry_d;
 
 endmodule
 
@@ -31,13 +28,12 @@ endmodule
 // Task
 //----------------------------------------------------------------------------
 
-module serial_adder_using_logic_operations_only
-(
-  input  clk,
-  input  rst,
-  input  a,
-  input  b,
-  output sum
+module serial_adder_using_logic_operations_only (
+    input  clk,
+    input  rst,
+    input  a,
+    input  b,
+    output sum
 );
 
   // Task:
@@ -49,6 +45,12 @@ module serial_adder_using_logic_operations_only
   // for information about the 1-bit full adder implementation.
   //
   // See the testbench for the output format ($display task).
+  logic carry;
 
+  always_ff @(posedge clk)
+    if (rst) carry <= '0;
+    else carry <=  a & b;
+
+  assign sum = (a ^ b ^ carry);
 
 endmodule
