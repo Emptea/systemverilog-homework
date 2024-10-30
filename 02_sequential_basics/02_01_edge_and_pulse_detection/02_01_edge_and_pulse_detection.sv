cd42794ffa4b2119ed5b619829459bacb2266bd5
Emptea <aryas1725@gmail.com>
2024-10-30 23:47:42 +0300

    pass 1 & 2

--- 02_sequential_basics/02_01_edge_and_pulse_detection/02_01_edge_and_pulse_detection.sv
+++ 02_sequential_basics/02_01_edge_and_pulse_detection/02_01_edge_and_pulse_detection.sv
@@ -2,7 +2,12 @@
 // Example
 //----------------------------------------------------------------------------
 
-module posedge_detector (input clk, rst, a, output detected);
+module posedge_detector (
+    input  clk,
+    rst,
+    a,
+    output detected
+);
 
   logic a_r;
 
@@ -10,13 +15,11 @@ module posedge_detector (input clk, rst, a, output detected);
   // The a_r flip-flop input value d propogates to the output q
   // only on the next clock cycle.
 
-  always_ff @ (posedge clk)
-    if (rst)
-      a_r <= '0;
-    else
-      a_r <= a;
+  always_ff @(posedge clk)
+    if (rst) a_r <= '0;
+    else a_r <= a;
 
-  assign detected = ~ a_r & a;
+  assign detected = ~a_r & a;
 
 endmodule
 
@@ -24,13 +27,50 @@ endmodule
 // Task
 //----------------------------------------------------------------------------
 
-module one_cycle_pulse_detector (input clk, rst, a, output detected);
+module one_cycle_pulse_detector (
+    input  clk,
+    rst,
+    a,
+    output detected
+);
 
+  typedef enum bit [1:0] {
+    S0 = 2'd0,
+    S1 = 2'd1,
+    S2 = 2'd2,
+    S3 = 2'd3
+  } state_e;
+
+  state_e state, next_state;
   // Task:
   // Create an one cycle pulse (010) detector.
   //
   // Note:
   // See the testbench for the output format ($display task).
+
+  // State register
+
+  always_ff @(posedge clk or posedge rst)
+    if (rst) state <= S0;
+    else state <= next_state;
+
+
+  // Next state logic
+
+  always_comb begin
+    next_state = state;
+
+    case (state)
+      S0: if (~a) next_state = S1;
+      S1: if (a) next_state = S2;
+      S2: next_state = S0;
+      default: next_state = S0;
+    endcase
+  end
+
+  // Output logic based on current state
+
+  assign detected = ( ~a & state == S2);
 
 
 endmodule
--- 02_sequential_basics/02_02_single_and_double_rate_fibonacci/02_02_single_and_double_rate_fibonacci.sv
+++ 02_sequential_basics/02_02_single_and_double_rate_fibonacci/02_02_single_and_double_rate_fibonacci.sv
@@ -2,20 +2,17 @@
 // Example
 //----------------------------------------------------------------------------
 
-module fibonacci
-(
-  input               clk,
-  input               rst,
-  output logic [15:0] num
+module fibonacci (
+    input               clk,
+    input               rst,
+    output logic [15:0] num
 );
 
   logic [15:0] num2;
 
-  always_ff @ (posedge clk)
-    if (rst)
-      { num, num2 } <= { 16'd1, 16'd1 };
-    else
-      { num, num2 } <= { num2, num + num2 };
+  always_ff @(posedge clk)
+    if (rst) {num, num2} <= {16'd1, 16'd1};
+    else {num, num2} <= {num2, num + num2};
 
 endmodule
 
@@ -23,16 +20,20 @@ endmodule
 // Task
 //----------------------------------------------------------------------------
 
-module fibonacci_2
-(
-  input               clk,
-  input               rst,
-  output logic [15:0] num,
-  output logic [15:0] num2
+module fibonacci_2 (
+    input               clk,
+    input               rst,
+    output logic [15:0] num,
+    output logic [15:0] num2
 );
 
   // Task:
   // Implement a module that generates two fibonacci numbers per cycle
+  logic [15:0] prev_num1, prev_num2;
+
+  always_ff @(posedge clk)
+    if (rst) {prev_num2, prev_num1, num, num2} <= {16'd0, 16'd0, 16'd1, 16'd1};
+    else {prev_num2, prev_num1, num, num2} <= {num, num + prev_num1, num + num2, num + num2 + num2};
 
 
 endmodule
