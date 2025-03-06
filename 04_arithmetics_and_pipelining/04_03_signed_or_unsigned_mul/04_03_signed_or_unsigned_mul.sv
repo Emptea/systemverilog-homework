6eccc0c3672a14815ddba0a8b7cce64223e07375
Emptea <aryas1725@gmail.com>
2025-03-07 00:24:15 +0300

    04_03 done

--- 04_arithmetics_and_pipelining/04_03_signed_or_unsigned_mul/04_03_signed_or_unsigned_mul.sv
+++ 04_arithmetics_and_pipelining/04_03_signed_or_unsigned_mul/04_03_signed_or_unsigned_mul.sv
@@ -6,10 +6,10 @@
 // that implements the signed multiplication of 4-bit numbers
 // which produces 8-bit result
 
-module signed_mul_4
-(
-  input  signed [3:0] a, b,
-  output signed [7:0] res
+module signed_mul_4 (
+    input  signed [3:0] a,
+    b,
+    output signed [7:0] res
 );
 
   assign res = a * b;
@@ -20,13 +20,12 @@ endmodule
 // that implements the unsigned multiplication of N-bit numbers
 // which produces 2N-bit result
 
-module unsigned_mul
-# (
-  parameter n = 8
-)
-(
-  input  [    n - 1:0] a, b,
-  output [2 * n - 1:0] res
+module unsigned_mul #(
+    parameter n = 8
+) (
+    input  [    n - 1:0] a,
+    b,
+    output [2 * n - 1:0] res
 );
 
   assign res = a * b;
@@ -43,14 +42,23 @@ endmodule
 // that produces either signed or unsigned result
 // of the multiplication depending on the 'signed_mul' input bit.
 
-module signed_or_unsigned_mul
-# (
-  parameter n = 8
-)
-(
-  input  [    n - 1:0] a, b,
-  input                signed_mul,
-  output [2 * n - 1:0] res
+module signed_or_unsigned_mul #(
+    parameter n = 8
+) (
+    input  [    n - 1:0] a,
+    b,
+    input                signed_mul,
+    output [2 * n - 1:0] res
 );
+  logic [2 * n - 1:0] out;
+  logic signed [n - 1:0] a_signed, b_signed;
+  assign a_signed = a;
+  assign b_signed = b;
 
+  always_comb begin
+    if (signed_mul) out = a_signed * b_signed;
+    else out = a * b;
+  end
+
+  assign res = out;
 endmodule
