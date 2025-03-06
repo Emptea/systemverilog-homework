7ac594b181780e66f0939afaee221aef3ceef251
Emptea <aryas1725@gmail.com>
2025-03-07 00:42:54 +0300

    04_05 done

--- 04_arithmetics_and_pipelining/04_05_circular_shifts/04_05_circular_shifts.sv
+++ 04_arithmetics_and_pipelining/04_05_circular_shifts/04_05_circular_shifts.sv
@@ -2,17 +2,25 @@
 // Example
 //----------------------------------------------------------------------------
 
-module circular_left_shift_of_N_by_S_using_bit_slices_and_concatenation
-# (parameter N = 8, S = 3)
-(input  [N - 1:0] a, output [N - 1:0] res);
+module circular_left_shift_of_N_by_S_using_bit_slices_and_concatenation #(
+    parameter N = 8,
+    S = 3
+) (
+    input  [N - 1:0] a,
+    output [N - 1:0] res
+);
 
-  assign res = { a [N - S - 1:0], a [N - 1 : N - S] };
+  assign res = {a[N-S-1:0], a[N-1 : N-S]};
 
 endmodule
 
-module circular_left_shift_of_N_by_S_by_ORing_the_results_of_shift_operations
-# (parameter N = 8, S = 3)
-(input  [N - 1:0] a, output [N - 1:0] res);
+module circular_left_shift_of_N_by_S_by_ORing_the_results_of_shift_operations #(
+    parameter N = 8,
+    S = 3
+) (
+    input  [N - 1:0] a,
+    output [N - 1:0] res
+);
 
   assign res = (a << S) | (a >> (N - S));
 
@@ -22,9 +30,13 @@ endmodule
 // Task
 //----------------------------------------------------------------------------
 
-module circular_right_shift_of_N_by_S_using_bit_slices_and_concatenation
-# (parameter N = 8, S = 3)
-(input  [N - 1:0] a, output [N - 1:0] res);
+module circular_right_shift_of_N_by_S_using_bit_slices_and_concatenation #(
+    parameter N = 8,
+    S = 3
+) (
+    input  [N - 1:0] a,
+    output [N - 1:0] res
+);
 
   // Task:
   //
@@ -32,13 +44,18 @@ module circular_right_shift_of_N_by_S_using_bit_slices_and_concatenation
   // in a circular fashion, using only concatenation of bit slices.
   //
   // "Circular" means ABCDEFGH -> FGHABCDE when N = 8 and S = 3.
-
+  // assign res = {a[N-S-1:0], a[N-1 : N-S]};
+  assign res = {a[S-1:0], a[N-1 : N-S-2]};
 
 endmodule
 
-module circular_right_shift_of_N_by_S_by_ORing_the_results_of_shift_operations
-# (parameter N = 8, S = 3)
-(input  [N - 1:0] a, output [N - 1:0] res);
+module circular_right_shift_of_N_by_S_by_ORing_the_results_of_shift_operations #(
+    parameter N = 8,
+    S = 3
+) (
+    input  [N - 1:0] a,
+    output [N - 1:0] res
+);
 
   // Task:
   //
@@ -48,6 +65,7 @@ module circular_right_shift_of_N_by_S_by_ORing_the_results_of_shift_operations
   // "or" (|) and constant expressions.
   //
   // "Circular" means ABCDEFGH -> FGHABCDE when N = 8 and S = 3.
+  assign res = (a << (N-S) | (a >>  S));
 
 
 endmodule
