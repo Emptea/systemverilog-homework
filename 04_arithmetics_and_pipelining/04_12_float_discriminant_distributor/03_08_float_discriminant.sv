285cf006935eb13dbd3806af4f32ca6254d2639b
Emptea <aryas1725@gmail.com>
2025-03-15 17:46:21 +0300

    04_12 done

--- 04_arithmetics_and_pipelining/04_12_float_discriminant_distributor/03_08_float_discriminant.sv
+++ 04_arithmetics_and_pipelining/04_12_float_discriminant_distributor/03_08_float_discriminant.sv
@@ -3,32 +3,90 @@
 //----------------------------------------------------------------------------
 
 module float_discriminant (
-    input                     clk,
-    input                     rst,
+    input clk,
+    input rst,
 
-    input                     arg_vld,
-    input        [FLEN - 1:0] a,
-    input        [FLEN - 1:0] b,
-    input        [FLEN - 1:0] c,
+    input              arg_vld,
+    input [FLEN - 1:0] a,
+    input [FLEN - 1:0] b,
+    input [FLEN - 1:0] c,
 
     output logic              res_vld,
     output logic [FLEN - 1:0] res,
     output logic              res_negative,
     output logic              err,
 
-    output logic              busy
+    output logic busy
 );
 
-    // Task:
-    // Implement a module that accepts three Floating-Point numbers and outputs their discriminant.
-    // The resulting value res should be calculated as a discriminant of the quadratic polynomial.
-    // That is, res = b^2 - 4ac == b*b - 4*a*c
-    //
-    // Note:
-    // If any argument is not a valid number, that is NaN or Inf, the "err" flag should be set.
-    //
-    // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
-    // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
+  // Task:
+  // Implement a module that accepts three Floating-Point numbers and outputs their discriminant.
+  // The resulting value res should be calculated as a discriminant of the quadratic polynomial.
+  // That is, res = b^2 - 4ac == b*b - 4*a*c
+  //
+  // Note:
+  // If any argument is not a valid number, that is NaN or Inf, the "err" flag should be set.
+  //
+  // The FLEN parameter is defined in the "import/preprocessed/cvw/config-shared.vh" file
+  // and usually equal to the bit width of the double-precision floating-point number, FP64, 64 bits.
 
+  logic [FLEN - 1:0] b_sq, ac, four_ac;
+  logic down_valid_b_sq, down_valid_ac, down_valid_four_ac;
+  logic busy_b_sq, busy_ac, busy_sub, busy_four_ac;
+  logic err_b_sq, err_ac, err_sub, err_four_ac;
+
+  localparam [FLEN - 1:0] four = 64'h4010_0000_0000_0000;
+
+
+  f_mult i_b_sq (
+      .clk(clk),
+      .rst(rst),
+      .a(b),
+      .b(b),
+      .up_valid(arg_vld),
+      .res(b_sq),
+      .down_valid(down_valid_b_sq),
+      .busy(busy_b_sq),
+      .error(err_b_sq)
+  );
+
+  f_mult i_ac (
+      .clk(clk),
+      .rst(rst),
+      .a(a),
+      .b(c),
+      .up_valid(arg_vld),
+      .res(ac),
+      .down_valid(down_valid_ac),
+      .busy(busy_ac),
+      .error(err_ac)
+  );
+
+  f_mult i_four_ac (
+      .clk(clk),
+      .rst(rst),
+      .a(four),
+      .b(ac),
+      .up_valid(down_valid_ac),
+      .res(four_ac),
+      .down_valid(down_valid_four_ac),
+      .busy(busy_four_ac),
+      .error(err_four_ac)
+  );
+
+  f_sub i_discr (
+      .clk(clk),
+      .rst(rst),
+      .a(b_sq),
+      .b(four_ac),
+      .up_valid(down_valid_four_ac),
+      .res(res),
+      .down_valid(res_vld),
+      .busy(busy_sub),
+      .error(err_sub)
+  );
+
+  assign busy = busy_ac | busy_b_sq | busy_sub;
+  assign err  = err_ac | err_b_sq | err_sub;
 
 endmodule
--- 04_arithmetics_and_pipelining/04_12_float_discriminant_distributor/04_12_float_discriminant_distributor.sv
+++ 04_arithmetics_and_pipelining/04_12_float_discriminant_distributor/04_12_float_discriminant_distributor.sv
@@ -1,34 +1,109 @@
-module float_discriminant_distributor (
-    input                           clk,
-    input                           rst,
+module float_discriminant_distributor #(
+    n_pipe_stages = 4
+) (
+    input clk,
+    input rst,
 
-    input                           arg_vld,
-    input        [FLEN - 1:0]       a,
-    input        [FLEN - 1:0]       b,
-    input        [FLEN - 1:0]       c,
+    input              arg_vld,
+    input [FLEN - 1:0] a,
+    input [FLEN - 1:0] b,
+    input [FLEN - 1:0] c,
 
-    output logic                    res_vld,
-    output logic [FLEN - 1:0]       res,
-    output logic                    res_negative,
-    output logic                    err,
+    output logic              res_vld,
+    output logic [FLEN - 1:0] res,
+    output logic              res_negative,
+    output logic              err,
 
-    output logic                    busy
+    output logic busy
 );
 
-    // Task:
-    //
-    // Implement a module that will calculate the discriminant based
-    // on the triplet of input number a, b, c. The module must be pipelined.
-    // It should be able to accept a new triple of arguments on each clock cycle
-    // and also, after some time, provide the result on each clock cycle.
-    // The idea of the task is similar to the task 04_11. The main difference is
-    // in the underlying module 03_08 instead of formula modules.
-    //
-    // Note 1:
-    // Reuse your file "03_08_float_discriminant.sv" from the Homework 03.
-    //
-    // Note 2:
-    // Latency of the module "float_discriminant" should be clarified from the waveform.
+  // Task:
+  //
+  // Implement a module that will calculate the discriminant based
+  // on the triplet of input number a, b, c. The module must be pipelined.
+  // It should be able to accept a new triple of arguments on each clock cycle
+  // and also, after some time, provide the result on each clock cycle.
+  // The idea of the task is similar to the task 04_11. The main difference is
+  // in the underlying module 03_08 instead of formula modules.
+  //
+  // Note 1:
+  // Reuse your file "03_08_float_discriminant.sv" from the Homework 03.
+  //
+  // Note 2:
+  // Latency of the module "float_discriminant" should be clarified from the waveform.
+
+  // COUNTER
+  logic [$clog2(n_pipe_stages)-1:0] cnt;
+
+  always_ff @(posedge clk) begin
+    if (rst) cnt <= '0;
+    else if (arg_vld) cnt <= cnt + 1;
+  end
+
+  // LOGIC FOR IN
+  logic [FLEN - 1:0] data_a[n_pipe_stages];
+  logic [FLEN - 1:0] data_b[n_pipe_stages];
+  logic [FLEN - 1:0] data_c[n_pipe_stages];
+  logic [n_pipe_stages -1 : 0] arg_vld_for_each_module;
+  generate
+    genvar i;
+    for (i = 0; i < n_pipe_stages; i++) begin
+      always_ff @(posedge clk) begin
+        if (rst) arg_vld_for_each_module[i] <= '0;
+        else if (cnt == i) arg_vld_for_each_module[i] <= arg_vld;
+        else arg_vld_for_each_module[i] <= '0;
 
 
+        if (arg_vld & (cnt == i)) begin
+          data_a[i] <= a;
+          data_b[i] <= b;
+          data_c[i] <= c;
+        end
+      end
+    end
+  endgenerate
+
+  // LOGIC FOR OUT
+  logic [FLEN - 1:0] data_res[n_pipe_stages];
+  logic [FLEN - 1:0] res_out;
+  logic [n_pipe_stages -1 : 0] res_vld_for_each_module;
+  logic [n_pipe_stages -1 : 0] res_negative_for_each_module;
+  logic [n_pipe_stages -1 : 0] err_for_each_module;
+  logic [n_pipe_stages -1 : 0] busy_for_each_module;
+
+
+  always_comb begin
+    for (int j = 0; j < n_pipe_stages; j++) begin
+      if (res_vld_for_each_module == (1 << j)) begin
+        res_out = data_res[j];
+      end
+    end
+  end
+  assign res_vld = |res_vld_for_each_module;
+  assign res_negative = |res_negative_for_each_module;
+  assign err = |err_for_each_module;
+  assign busy = |busy_for_each_module;
+  assign res = res_out;
+
+  generate
+    for (i = 0; i < n_pipe_stages; i++) begin
+
+      float_discriminant fd (
+          .clk(clk),
+          .rst(rst),
+
+          .arg_vld(arg_vld_for_each_module[i]),
+          .a(data_a[i]),
+          .b(data_b[i]),
+          .c(data_c[i]),
+
+          .res_vld(res_vld_for_each_module[i]),
+          .res(data_res[i]),
+          .res_negative(res_negative_for_each_module[i]),
+          .err(err_for_each_module[i]),
+
+          .busy(busy_for_each_module[i])
+      );
+    end
+  endgenerate
 endmodule
